c240b2fd4184f10207b59f53d3dcae036b88e081
karttofer <jhornancolina@gmail.com>
2024-12-23 10:07:19 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterOutlet } from "@angular/router";
+import { PasswordModule } from "primeng/password";
+import { FloatLabelModule } from "primeng/floatlabel";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	imports: [RouterOutlet, FormsModule, PasswordModule, FloatLabelModule],
+	standalone: true,
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'fluyent';
+	value1!: string;
+
+	value2!: string;
+
+	value3!: string;
 }
