7ec778275bce763fe6d0f0d7c446c85eac9f8496
karttofer <jhornancolina@gmail.com>
2024-12-23 15:14:44 -0500

    back to initial config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { RouterOutlet } from "@angular/router";
-import { PasswordModule } from "primeng/password";
-import { FloatLabelModule } from "primeng/floatlabel";
 
 @Component({
 	selector: "app-root",
-	imports: [RouterOutlet, FormsModule, PasswordModule, FloatLabelModule],
+	imports: [RouterOutlet],
 	standalone: true,
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
